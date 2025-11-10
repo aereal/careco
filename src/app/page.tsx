07a366aec717b47e884b1f4d3990fd75ac17cb3c
aereal <trasty.loose@gmail.com>
2025-11-10 16:23:35 +0900

    refactor(front): use Result operators

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,29 @@
 import { SelectMonth } from '@/components/SelectMonth';
 import { YMD } from '@/components/YMD';
 import { getClient } from '@/get-client';
+import { readQuery } from '@/run-operation';
+import { Result } from '@praha/byethrow';
 import { FC } from 'react';
 import { GetRoot } from './query.get-root';
 
 const Page: FC = async () => {
-  const { data, error } = await getClient().query(GetRoot, { first: 5 });
-  if (error) {
-    return <>Error: {error.message}</>;
+  const ret = await readQuery(getClient(), GetRoot)({ first: 5 });
+  if (Result.isFailure(ret)) {
+    return <>Error: {ret.error.message}</>;
   }
-  if (!data) {
-    return null;
-  }
+  const {
+    value: {
+      data: { totalStatistics, recentDrivingRecords },
+    },
+  } = ret;
   return (
     <div>
       <div>
         <SelectMonth />
       </div>
-      <div>total: {data.totalStatistics.distanceKilometers}km</div>
+      <div>total: {totalStatistics.distanceKilometers}km</div>
       <ul>
-        {data.recentDrivingRecords.map((record) => (
+        {recentDrivingRecords.map((record) => (
           <li key={record.recordedAt.toString()}>
             <YMD date={record.recordedAt} />: {record.distanceKilometers}km
           </li>
