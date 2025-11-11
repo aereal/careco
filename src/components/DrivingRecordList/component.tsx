f4023c5203186cfe8aebea235db992c8572f6326
aereal <trasty.loose@gmail.com>
2025-11-11 09:09:35 +0900

    fix(front): use table

--- src/components/DrivingRecordList/component.tsx
+++ src/components/DrivingRecordList/component.tsx
@@ -8,12 +8,23 @@ export const DrivingRecordList: FC<FragmentType<typeof fragmentRecordList>> = (
 ) => {
   const { nodes } = getFragmentData(fragmentRecordList, props);
   return (
-    <ol className='mb-8 list rounded-box shadow-md'>
-      {nodes.map((record) => (
-        <li key={record.recordedAt.toString()} className='p-2'>
-          <YMD date={record.recordedAt} />: {record.distanceKilometers}km
-        </li>
-      ))}
-    </ol>
+    <table className='table'>
+      <thead>
+        <tr>
+          <th>日付</th>
+          <th>走行距離</th>
+        </tr>
+      </thead>
+      <tbody>
+        {nodes.map(({ recordedAt, distanceKilometers }) => (
+          <tr key={recordedAt.toString()}>
+            <td>
+              <YMD date={recordedAt} />
+            </td>
+            <td>{distanceKilometers}km</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
