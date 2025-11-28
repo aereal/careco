a1d3657ebe76d7734c77a91d07e98691498a3772
aereal <trasty.loose@gmail.com>
2025-11-28 20:37:26 +0900

    refactor(back): startOfMonth helper

--- backend/timeops/ops.go
+++ backend/timeops/ops.go
@@ -4,11 +4,15 @@ import "time"
 
 func StartOfNextMonth(t time.Time) time.Time {
 	if t.Month() == time.December {
-		return time.Date(t.Year()+1, time.January, 1, 0, 0, 0, 0, t.Location())
+		return startOfMonth(t.Year()+1, time.January, t.Location())
 	}
-	return time.Date(t.Year(), t.Month()+1, 1, 0, 0, 0, 0, t.Location())
+	return startOfMonth(t.Year(), t.Month()+1, t.Location())
 }
 
 func StartOfNextYear(t time.Time) time.Time {
-	return time.Date(t.Year()+1, time.January, 1, 0, 0, 0, 0, t.Location())
+	return startOfMonth(t.Year()+1, time.January, t.Location())
+}
+
+func startOfMonth(year int, month time.Month, loc *time.Location) time.Time {
+	return time.Date(year, month, 1, 0, 0, 0, 0, loc)
 }
