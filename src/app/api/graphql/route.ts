b337485f88ac168dfaee07dbc07e980169c0ba50
aereal <trasty.loose@gmail.com>
2025-12-07 15:02:22 +0900

    ci: pass Auth0 SDK-related environment variables

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,4 +1,5 @@
 import { client } from '@/lib/auth0';
+import { Result } from '@praha/byethrow';
 import { NextRequest, NextResponse } from 'next/server';
 
 const upstreamEndpoint = 'http://localhost:8080/graphql';
@@ -9,13 +10,8 @@ class InternalUnauthenticatedError extends Error {
   }
 }
 
-export const POST = async (
-  req: NextRequest,
-): Promise<NextResponse<unknown>> => {
-  const downstreamBody = await req.json();
-  const headers = new Headers(req.headers);
-  headers.set('content-type', 'application/json');
-  try {
+const getAccessToken = Result.try({
+  async try() {
     const session = await client.getSession();
     if (!session) {
       throw new InternalUnauthenticatedError();
@@ -23,17 +19,75 @@ export const POST = async (
     const {
       tokenSet: { accessToken },
     } = session;
-    headers.set('authorization', `Bearer ${accessToken}`);
-  } catch {}
-  const init: RequestInit = {
-    method: 'POST',
-    headers,
-    body: JSON.stringify(downstreamBody),
-  };
-  const upstreamResp = await fetch(upstreamEndpoint, init);
-  const upstreamBody = await upstreamResp.json();
-  return NextResponse.json(upstreamBody, {
-    status: upstreamResp.status,
-    headers: upstreamResp.headers,
-  });
+    return accessToken;
+  },
+  catch(err: unknown): InternalUnauthenticatedError | Error {
+    if (err instanceof InternalUnauthenticatedError) {
+      return err;
+    } else if (err instanceof Error) {
+      return err;
+    } else {
+      return new Error(`${err}`);
+    }
+  },
+});
+
+interface RequestBody {
+  body: unknown;
+}
+
+const getRequestBody = Result.try({
+  try: async (req: NextRequest) => ({ body: await req.json() }) as RequestBody,
+  catch: (err: unknown) => (err instanceof Error ? err : new Error(`${err}`)),
+});
+
+const doRequest = Result.try({
+  async try(args: { headers: Headers; downstreamBody: RequestBody }) {
+    const init: RequestInit = {
+      method: 'POST',
+      headers: args.headers,
+      body: JSON.stringify(args.downstreamBody.body),
+    };
+    const upstreamResp = await fetch(upstreamEndpoint, init);
+    const upstreamBody = await upstreamResp.json();
+    return NextResponse.json<unknown>(upstreamBody, {
+      status: upstreamResp.status,
+      headers: upstreamResp.headers,
+    });
+  },
+  catch: (err: unknown) => (err instanceof Error ? err : new Error(`${err}`)),
+});
+
+export const POST = async (
+  req: NextRequest,
+): Promise<NextResponse<unknown>> => {
+  const ret = await Result.pipe(
+    Result.sequence({
+      downstreamBody: getRequestBody(req),
+      headers: Result.pipe(
+        Result.sequence({
+          headers: Result.succeed(req.headers),
+          accessToken: getAccessToken(),
+        }),
+        Result.inspect(({ accessToken, headers }) => {
+          headers.set('authorization', `Bearer ${accessToken}`);
+        }),
+        Result.map(({ headers }) => headers),
+      ),
+    }),
+    Result.andThen(doRequest),
+    Result.orElse((err) =>
+      Result.succeed(
+        NextResponse.json<unknown>({ error: `${err}` }, { status: 401 }),
+      ),
+    ),
+  );
+  if (Result.isFailure(ret)) {
+    console.error(`! unexpected error: ${ret.error}`);
+    return NextResponse.json(
+      { error: 'unexpected error occurred' },
+      { status: 500 },
+    );
+  }
+  return ret.value;
 };
