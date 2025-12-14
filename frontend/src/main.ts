b3034051e844bbf6b3374937671554d5552c9813
aereal <trasty.loose@gmail.com>
2025-12-14 21:15:43 +0900

    fix(front): setup Urql client

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,4 @@
+import urql, { fetchExchange } from '@urql/vue';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
@@ -5,5 +6,10 @@ import router from './router';
 const app = createApp(App);
 
 app.use(router);
+app.use(urql, {
+  url: 'http://localhost:8080/graphql',
+  exchanges: [fetchExchange],
+  preferGetMethod: false,
+});
 
 app.mount('#app');
