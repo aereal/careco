0f92d295988065ad9c81a9daf149e493a1172d9d
aereal <trasty.loose@gmail.com>
2025-12-14 20:40:50 +0900

    feat(front): setup routing

---
+++ frontend/src/MonthlyReportPage.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>{{ $route.params.year }}/{{ $route.params.month }} report</h1>
+</template>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,19 @@
+import MonthlyReportPage from '@/MonthlyReportPage.vue';
+import RootPage from '@/RootPage.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      component: RootPage,
+    },
+    {
+      path: '/reports/:year/:month',
+      component: MonthlyReportPage,
+    },
+  ],
 });
 
 export default router;
