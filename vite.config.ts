db14cf63e94586b0bb99ed2e37ab17a1627ab5e1
aereal <trasty.loose@gmail.com>
2025-12-15 20:10:12 +0900

    fix(front): setup tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { fileURLToPath, URL } from 'node:url';
-
+import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./frontend/src', import.meta.url)),
