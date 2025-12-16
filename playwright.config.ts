e28b1da506ed0e47eca647d8d901ec22bc9de153
aereal <trasty.loose@gmail.com>
2025-12-17 08:40:28 +0900

    test(front): pnpm build

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: inCI ? 'pnpm run start' : 'pnpm run dev',
+    command: inCI ? 'pnpm build && pnpm start' : 'pnpm run dev',
     url: baseURL,
     reuseExistingServer: !inCI,
     timeout: 120 * 1000,
