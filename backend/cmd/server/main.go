e8d2c846d81191213ce3a611fc633e12d55df7e0
aereal <trasty.loose@gmail.com>
2025-12-18 10:53:28 +0900

    refactor(back): extract Start function

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,48 +1,9 @@
 package main
 
 import (
-	"context"
-	"errors"
-	"log/slog"
 	"os"
 
-	"careco/backend/log/attribute"
+	"careco/backend/entrypoint/server"
 )
 
-func main() { os.Exit(do()) }
-
-func do() int {
-	return exitCodeOf(run(context.Background()))
-}
-
-func run(ctx context.Context) error {
-	entrypoint, err := build(ctx)
-	if err != nil {
-		return err
-	}
-	defer entrypoint.Shutdown(context.WithoutCancel(ctx))
-	return entrypoint.Start(ctx)
-}
-
-func exitCodeOf(err error) (exitCode int) {
-	defer func() {
-		level := slog.LevelDebug
-		if exitCode > 0 {
-			level = slog.LevelError
-		}
-		attrs := make([]slog.Attr, 0, 2)
-		attrs = append(attrs, slog.Int("exit_code", exitCode))
-		if err != nil {
-			attrs = append(attrs, attribute.Error(err))
-		}
-		slog.LogAttrs(context.Background(), level, "application exited", attrs...)
-	}()
-	if err == nil {
-		return 0
-	}
-	var hasExitCode interface{ ExitCode() int }
-	if errors.As(err, &hasExitCode) {
-		return hasExitCode.ExitCode()
-	}
-	return 1
-}
+func main() { os.Exit(server.Start(build)) }
