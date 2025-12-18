c2b25c612d9a008054eb60609ba5c0739a753e6b
aereal <trasty.loose@gmail.com>
2025-12-18 10:12:09 +0900

    fix(back): emits error and exit status (#69)

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"context"
 	"errors"
+	"log/slog"
 	"os"
+
+	"careco/backend/log/attribute"
 )
 
 func main() { os.Exit(do()) }
@@ -21,7 +24,19 @@ func run(ctx context.Context) error {
 	return entrypoint.Start(ctx)
 }
 
-func exitCodeOf(err error) int {
+func exitCodeOf(err error) (exitCode int) {
+	defer func() {
+		level := slog.LevelDebug
+		if exitCode > 0 {
+			level = slog.LevelError
+		}
+		attrs := make([]slog.Attr, 0, 2)
+		attrs = append(attrs, slog.Int("exit_code", exitCode))
+		if err != nil {
+			attrs = append(attrs, attribute.Error(err))
+		}
+		slog.LogAttrs(context.Background(), level, "application exited", attrs...)
+	}()
 	if err == nil {
 		return 0
 	}
