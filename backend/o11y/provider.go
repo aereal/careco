bcb3b2f8a92a49a3f5ed1e627231e39bdfad1f81
aereal <trasty.loose@gmail.com>
2025-12-28 16:03:48 +0900

    fix(back): explicitly specify sampler

--- backend/o11y/provider.go
+++ backend/o11y/provider.go
@@ -59,6 +59,7 @@ func ProvideTracerProvider(ctx context.Context, exporter *otlptrace.Exporter, re
 	return sdktrace.NewTracerProvider(
 		sdktrace.WithResource(res),
 		sdktrace.WithBatcher(exporter),
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
 	), nil
 }
 
