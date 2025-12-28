4fc53e0a6ee2549575d04104be88738c1e577977
aereal <trasty.loose@gmail.com>
2025-12-28 16:19:17 +0900

    fix(back): log spans

--- backend/o11y/provider.go
+++ backend/o11y/provider.go
@@ -59,6 +59,7 @@ func ProvideTracerProvider(ctx context.Context, exporter *otlptrace.Exporter, re
 	return sdktrace.NewTracerProvider(
 		sdktrace.WithResource(res),
 		sdktrace.WithBatcher(exporter),
+		sdktrace.WithBatcher(&spanLogger{}),
 		sdktrace.WithSampler(sdktrace.AlwaysSample()),
 	), nil
 }
---
+++ backend/o11y/span_logger.go
@@ -0,0 +1,52 @@
+package o11y
+
+import (
+	"context"
+	"log/slog"
+	"slices"
+
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+)
+
+type spanLogger struct{}
+
+var _ sdktrace.SpanExporter = (*spanLogger)(nil)
+
+func (*spanLogger) ExportSpans(ctx context.Context, spans []sdktrace.ReadOnlySpan) error {
+	slog.DebugContext(ctx, "ExportSpans: start process spans", slog.Int("num", len(spans)))
+	sortedSpans := slices.SortedFunc(slices.Values(spans), func(a, b sdktrace.ReadOnlySpan) int { return a.StartTime().Compare(b.StartTime()) })
+	for _, span := range sortedSpans {
+		if !span.SpanContext().IsValid() {
+			slog.DebugContext(ctx, "invalid span",
+				slog.String("name", span.Name()),
+				slog.String("instrumentation.name", span.InstrumentationScope().Name),
+			)
+			continue
+		}
+		isRoot := !span.Parent().IsValid()
+		switch {
+		case !span.SpanContext().IsSampled():
+			slog.DebugContext(ctx, "not-sampled span",
+				slog.String("name", span.Name()),
+				slog.Bool("is_root", isRoot),
+				slog.String("trace_id", span.SpanContext().TraceID().String()),
+				slog.String("span_id", span.SpanContext().SpanID().String()),
+				slog.String("instrumentation.name", span.InstrumentationScope().Name),
+			)
+		default:
+			slog.DebugContext(ctx, "valid span",
+				slog.String("name", span.Name()),
+				slog.Bool("is_root", isRoot),
+				slog.String("trace_id", span.SpanContext().TraceID().String()),
+				slog.String("span_id", span.SpanContext().SpanID().String()),
+				slog.String("instrumentation.name", span.InstrumentationScope().Name),
+			)
+		}
+	}
+	return nil
+}
+
+func (*spanLogger) Shutdown(ctx context.Context) error {
+	slog.DebugContext(ctx, "shutting down spanLogger")
+	return nil
+}
